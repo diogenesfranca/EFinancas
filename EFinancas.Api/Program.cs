80e2a75357794e76a0f92ca8b25cd28a91cd2fb8
diogenesfranca <diogenesfranca@outlook.com>
2023-07-31 20:33:39 -0300

    Uso de DateOnlyTimeOnly.AspNet.Swashbuckle para reconhecer o DateOnly no formato string no swagger

--- EFinancas.Api/Program.cs
+++ EFinancas.Api/Program.cs
@@ -18,6 +18,7 @@ namespace EFinancas.Api
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.AddDateOnlyTimeOnlyStringConverters();
 
             builder.Services.ConfigurarMongo();
             
