224eb5acd690b91f12b64cc7b346e3eefc0b006d
diogenesfranca <diogenesfranca@outlook.com>
2023-07-31 20:52:18 -0300

    Crianda classe base Financa para que Despesa e Receita herdem, definindo o nome das colunas de despesa e receita como camelCase

--- EFinancas.Dominio/Entidades/Despesa.cs
+++ EFinancas.Dominio/Entidades/Despesa.cs
@@ -1,18 +1,6 @@
-﻿using EFinancas.Dominio.Interfaces.Entidades;
-using MongoDB.Bson;
-using MongoDB.Bson.Serialization.Attributes;
-
-namespace EFinancas.Dominio.Entidades
+﻿namespace EFinancas.Dominio.Entidades
 {
-    internal class Despesa : IFinanca
-    {
-        [BsonId]
-        [BsonRepresentation(BsonType.ObjectId)]
-        public string Id { get; set; } = "";
-        public string Descricao { get; set; } = "";
-        public decimal Valor { get; set; }
-        public DateOnly Data { get; set; }
-        public string IdConta { get; set; } = "";
-        public IEnumerable<string> IdsCategorias { get; set; } = new List<string>();
+    public class Despesa : Financa
+    {        
     }
 }
---
+++ EFinancas.Dominio/Entidades/Financa.cs
@@ -0,0 +1,28 @@
+﻿using EFinancas.Dominio.Interfaces.Entidades;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace EFinancas.Dominio.Entidades
+{
+    public abstract class Financa : IFinanca
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; } = "";
+
+        [BsonElement("descricao", Order = 1)]
+        public string Descricao { get; set; } = "";
+
+        [BsonElement("valor", Order = 2)]
+        public decimal Valor { get; set; }
+
+        [BsonElement("data", Order = 3)]
+        public DateOnly Data { get; set; }
+
+        [BsonElement("idConta", Order = 4)]
+        public string IdConta { get; set; } = "";
+
+        [BsonElement("idsCategorias", Order = 5)]
+        public IEnumerable<string> IdsCategorias { get; set; } = new List<string>();
+    }
+}
