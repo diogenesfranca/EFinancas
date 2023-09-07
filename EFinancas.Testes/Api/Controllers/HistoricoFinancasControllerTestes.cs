e722f5c45c28395dd92a7ea4cbf3aef07e6fd905
diogenesfranca <diogenesfranca@outlook.com>
2023-09-07 20:33:21 -0300

    Melhoria dos testes unitários dos métodos Get da classe HistoricoFinancasController

--- EFinancas.Testes/Api/Controllers/HistoricoFinancasControllerTestes.cs
+++ EFinancas.Testes/Api/Controllers/HistoricoFinancasControllerTestes.cs
@@ -1,13 +1,10 @@
 ﻿using EFinancas.Api.Controllers;
 using EFinancas.Dominio.Interfaces.Servicos;
+using EFinancas.Dominio.Models;
+using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace EFinancas.Testes.Api.Controllers
@@ -26,22 +23,84 @@ namespace EFinancas.Testes.Api.Controllers
         [Fact]
         public async Task Get_Sucesso()
         {
+            //Preparação
+            listagemHistoricoFinancasServicoMock.Setup(x => x.Listar()).ReturnsAsync(new List<HistoricoFinanca>
+            {
+                new HistoricoFinanca
+                {
+                    Id = "12345",
+                    Descricao = "Salario",
+                    Data = DateOnly.FromDateTime(DateTime.Now.AddDays(-5)),
+                    Valor = 5456.32M
+                },
+                new HistoricoFinanca
+                {
+                    Id = "45647",
+                    Descricao = "Sorvete",
+                    Data = DateOnly.FromDateTime(DateTime.Now.AddDays(-4)),
+                    Valor = 12.36M
+                },
+            });
+
             //Ação
             var resultado = await controller.Get() as OkObjectResult;
 
             //Afirmação
             Assert.Equal((int)HttpStatusCode.OK, resultado!.StatusCode);
+
+            resultado.Value.Should().BeEquivalentTo(new List<HistoricoFinanca>
+            {
+                new HistoricoFinanca
+                {
+                    Id = "12345",
+                    Descricao = "Salario",
+                    Data = DateOnly.FromDateTime(DateTime.Now.AddDays(-5)),
+                    Valor = 5456.32M
+                },
+                new HistoricoFinanca
+                {
+                    Id = "45647",
+                    Descricao = "Sorvete",
+                    Data = DateOnly.FromDateTime(DateTime.Now.AddDays(-4)),
+                    Valor = 12.36M
+                },
+            });
+
             listagemHistoricoFinancasServicoMock.Verify(x => x.Listar(), Times.Once);
         }
 
         [Fact]
         public async Task Get_PorId_Sucesso()
         {
+            //Preparação
+            listagemHistoricoFinancasServicoMock.Setup(x => x.Listar("IdConta1")).ReturnsAsync(new List<HistoricoFinanca>
+            {
+                new HistoricoFinanca
+                {
+                    Id = "45647",
+                    Descricao = "Sorvete",
+                    Data = DateOnly.FromDateTime(DateTime.Now.AddDays(-4)),
+                    Valor = 12.36M
+                },
+            });
+
             //Ação
             var resultado = await controller.Get("IdConta1") as OkObjectResult;
 
             //Afirmação
             Assert.Equal((int)HttpStatusCode.OK, resultado!.StatusCode);
+
+            resultado.Value.Should().BeEquivalentTo(new List<HistoricoFinanca>
+            {
+                new HistoricoFinanca
+                {
+                    Id = "45647",
+                    Descricao = "Sorvete",
+                    Data = DateOnly.FromDateTime(DateTime.Now.AddDays(-4)),
+                    Valor = 12.36M
+                },
+            });
+
             listagemHistoricoFinancasServicoMock.Verify(x => x.Listar("IdConta1"), Times.Once);
         }
     }
